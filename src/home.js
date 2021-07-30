999dcf265d43b4cd072bc2df5c96419e68a35e02
Andreas Notokuusmo <andreasnotokusumo23@gmail.com>
2021-07-30 19:30:35 +0700

    :sparkles: useState Hooks

--- src/home.js
+++ src/home.js
@@ -1,10 +1,23 @@
+import { useState } from "react";
+
 const Home = () => {
 
-    const spell = {name: "Tasha's Hideous Laughter", type: "Abjuration"};
+    const [spells, addSpell] = useState([
+        {name: "Cure Wounds", type: "Invocation", id: "1"},
+        {name: "Heroism", type: "Enchantment", id: "2"},
+        {name: "Vicious Mockery", type: "Abjuration", id: "3"}
+    ]);
 
     return (
         <div className="home">
-            <h1>Dungeons Lore { spell.name }</h1>
+            {
+                spells.map((spell) => (
+                    <div className="spell-preview" key = {spell.id}>
+                        <h2>{ spell.name }</h2>
+                        <p>{ spell.type }</p>
+                    </div>
+                ))
+            }
         </div>
     );
 }
--- src/navbar.js
+++ src/navbar.js
@@ -1,10 +1,23 @@
+import { useState } from "react";
+
 const Navbar = () => {
+
     return (
         <nav className="navbar">
             <h1>Dungeons Lore</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create">New Spell</a>
+                <button
+                    href="/create"
+                    style={{
+                        color: "white",
+                        backgroundColor: "#E40511",
+                        borderRadius: "8px",
+                        padding: "10px 15px",
+                        border: "0px"
+                    }}
+                    onClick={() => {}}
+                >New Spell</button>
             </div>
         </nav>
     );
